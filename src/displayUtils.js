8aff9a6895622c408b033cda4bf3eaa904b98530
gilkur23 <122338710+gilkur23@users.noreply.github.com>
2024-09-08 10:14:51 +0700

    Update displayUtils.js

--- src/displayUtils.js
+++ src/displayUtils.js
@@ -5,7 +5,7 @@ function displayHeader() {
   console.log('========================================'.cyan);
   console.log('=           Sonic Odyssey BOT          ='.cyan);
   console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=    Edited by  SKW Airdrop Hunter     ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
