599031cea84fa657871b6b17b5a8012b4aa0754e
gilkur23 <122338710+gilkur23@users.noreply.github.com>
2024-09-15 14:58:47 +0700

    Create sendTelegramMessage.js

---
+++ sendTelegramMessage.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+const sendTelegramMessage = async (message) => {
+  try {
+    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+    const response = await axios.post(url, {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: message,
+    });
+
+    if (response.status === 200) {
+      console.log('Pesan berhasil dikirim ke Telegram.');
+    } else {
+      console.error('Gagal mengirim pesan ke Telegram. Status:', response.status);
+    }
+  } catch (error) {
+    console.error('Error sending message to Telegram:', error.message);
+  }
+};
+
+module.exports = { sendTelegramMessage };
